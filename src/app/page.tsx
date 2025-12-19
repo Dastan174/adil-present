e22b1257b8a2ef0254b6cbae71d491926a31a739
Dastan174 <kanbolotuuludas@gmail.com>
2025-12-19 18:36:24 +0600

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import React, { useEffect, useRef, useState } from "react";
+
+const page = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [showRest, setShowRest] = useState(false); // состояние для остальных блоков
+
+  const smoothScrollDown = () => {
+    setShowRest(true); // при клике открываем остальные блоки
+
+    const start = window.scrollY;
+    const target = start + window.innerHeight; // прокручиваем на высоту экрана
+    const duration = 1000;
+    let startTime: number | null = null;
+
+    const animate = (time: number) => {
+      if (!startTime) startTime = time;
+      const progress = Math.min((time - startTime) / duration, 1);
+
+      // easeOutCubic
+      const easing = 1 - Math.pow(1 - progress, 3);
+
+      window.scrollTo(0, start + (target - start) * easing);
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  };
+  useEffect(() => {
+    const enableSound = () => {
+      if (audioRef.current) {
+        audioRef.current.muted = false;
+        audioRef.current
+          .play()
+          .catch((e) => console.log("Ошибка воспроизведения:", e));
+      }
+    };
+    document.addEventListener("click", enableSound, { once: true });
+    document.addEventListener("scroll", enableSound, { once: true });
+    document.addEventListener("keydown", enableSound, { once: true });
+    return () => {
+      document.removeEventListener("click", enableSound);
+      document.removeEventListener("scroll", enableSound);
+      document.removeEventListener("keydown", enableSound);
+    };
+  }, []);
+  return (
+    <div className={styles.page}>
+      <audio ref={audioRef} loop>
+        <source src="/music.mp3" type="audio/mpeg" />
+        Ваш браузер не поддерживает аудио.
+      </audio>
+      <div onClick={smoothScrollDown} className={styles.img1}>
+        <Image priority src="/images/open.png" fill alt="open" />
+      </div>
+    </div>
+  );
+};
+
+export default page;
